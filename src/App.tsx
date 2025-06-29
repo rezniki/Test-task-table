d1eba743bdd1f59f8359f3691ee2c7bc41bcbdd8
rezniki <igorreznik2001@gmail.com>
2025-06-29 21:40:01 +0300

    App.tsx / add workpiece

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Layout } from 'antd';
 import './App.css';
+
+const { Header, Content } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout style={{ minHeight: '100vh' }}>
+      <Header style={{ color: '#fff', fontSize: '20px' }}>📊 Таблица данных</Header>
+      <Content style={{ padding: '24px' }}>
+        {/* Таблица будет тут */}
+      </Content>
+    </Layout>
   );
 }
 
